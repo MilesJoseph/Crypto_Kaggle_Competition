85867f2423b326b1100569bcc43e7c7e2b2fe2eb
EC2 Default User <ec2-user@ip-172-31-41-234.us-west-2.compute.internal>
2021-12-01 04:55:45 +0000

    new

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Crypto Competition on kaggle',
+    author='Miles Klingenberg',
+    license='MIT',
+)
